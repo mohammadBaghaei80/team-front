8b939fff4fae593964d5a6c12335083779c1f5f9
Yasin Satvati <57200251+theashozosht@users.noreply.github.com>
2022-08-02 10:50:32 +0430

    Update App.vue
    Test Case For Github

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from "vue-router";
 import Header from "./components/dashboard/Header.vue";
 import Sidebar from "./components/dashboard/Sidebar.vue";
 </script>
-
+<!-- Test Case For Github -->
 <template>
   <RouterView name="Header"></RouterView>
   <RouterView name="Sidebar"></RouterView>
